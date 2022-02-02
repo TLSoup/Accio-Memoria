2aa3bf21302a5823ce7486ba030fe05a7925a197
Tracy Campbell <tracy@tlsoup.com>
2022-02-01 21:35:38 -0700

    Bootsrap layout added to Create page

--- src/Screens/create.js
+++ src/Screens/create.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 export const Create = () => {
     return (
-        <h1>Create</h1>
+        <Container>
+            <Row>
+                <Col>
+                    <h1>Create</h1>
+                </Col>
+            </Row>
+        </Container>
     );
 }
