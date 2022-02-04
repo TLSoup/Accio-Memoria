3f68a9d76b0abed867fe286c6560b20131a1c438
Tracy Campbell <tracy@tlsoup.com>
2022-02-04 11:47:40 -0700

    basic form added to create page

--- src/Screens/create.js
+++ src/Screens/create.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 export const Create = () => {
     return (
         <Container>
             <Row>
                 <Col>
-                    <h1>Create</h1>
+                    <Form className="mt-4">
+                        <Form.Group className="mb-3" controlID="task">
+                            <Form.Control placeholder="Add Task" />
+                            <Form.Text className="text-muted">
+                                What would you like to remember?
+                            </Form.Text>
+                        </Form.Group>
+                        <Button variant="dark" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
                 </Col>
             </Row>
         </Container>
