2a6dad5a7d986494a83b625da1adc5cba534c4ca
Tracy Campbell <tracy@tlsoup.com>
2022-02-20 15:43:59 -0700

    font awesome added, placeholder user icon added, dark mode moon added

--- src/Components/navigation.js
+++ src/Components/navigation.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faCircleUser } from '@fortawesome/free-solid-svg-icons';
 
 export const Navigation = () => {
     return (
@@ -9,6 +11,15 @@ export const Navigation = () => {
                 <Nav className="me-auto">
                     <Nav.Link href="/create">Create</Nav.Link>
                     <Nav.Link href="/activity">Activity</Nav.Link>
+                </Nav>
+                <Nav className="pe-5">
+                    <Navbar.Text><FontAwesomeIcon icon={faMoon} /></Navbar.Text>
+                </Nav>
+                <Nav className="d-flex">
+                <Navbar.Text href="/"><FontAwesomeIcon icon={faCircleUser} /></Navbar.Text>
+                    <NavDropdown title="Tracy" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action4">Sign Out</NavDropdown.Item>
+                    </NavDropdown>
                 </Nav>
             </Container>
         </Navbar>
