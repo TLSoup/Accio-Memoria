d0c0ce54265404baf59db65446730986b3bf0309
Tracy Campbell <tracy@tlsoup.com>
2022-02-20 21:37:26 -0700

    updated routes in server

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('/users', require('./routes/users.routes'));
+app.use('/users', require('./routes/user.route'));
 app.use('/tasks', require('./routes/task.route'));
 
 app.get("/", (req, res) => {
